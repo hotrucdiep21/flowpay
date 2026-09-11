70bb6239fe00dd538824affe967a85ba34483fa1
TrucDev <hotrucdiep210203@gmail.com>
2026-09-11 17:15:53 +0700

    feat: complete core money transfer flow

--- src/main/java/com/flowpay/Main.java
+++ src/main/java/com/flowpay/Main.java
@@ -1,12 +1,69 @@
 package com.flowpay;
 
+import com.flowpay.infrastructure.persistence.inmemory.InMemoryTransferRepository;
+import com.flowpay.infrastructure.persistence.inmemory.InMemoryWalletRepository;
+import com.flowpay.transfer.application.TransferCommand;
+import com.flowpay.transfer.application.TransferService;
+import com.flowpay.transfer.domain.Transfer;
+import com.flowpay.wallet.domain.Money;
+import com.flowpay.wallet.domain.Wallet;
+
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        InMemoryWalletRepository walletRepository = new InMemoryWalletRepository();
+        InMemoryTransferRepository transferRepository = new InMemoryTransferRepository();
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        TransferService transferService = new TransferService(
+                walletRepository,
+                transferRepository
+        );
+
+        Wallet sender = new Wallet(
+                "wallet-an",
+                "user-an",
+                new Money(new BigDecimal("1000000"))
+        );
+
+        Wallet receiver = new Wallet(
+                "wallet-binh",
+                "user-binh",
+                new Money(new BigDecimal("200000"))
+        );
+
+        walletRepository.save(sender);
+        walletRepository.save(receiver);
+
+        System.out.println("Before Transfer");
+        printBalances(sender, receiver);
+        TransferCommand command = new TransferCommand(
+                "request-001",
+                "wallet-an",
+                "wallet-binh",
+                new Money(new BigDecimal("300000"))
+        );
+
+        Transfer transfer = transferService.transfer(command);
+
+        System.out.println("\nAfter transfer:");
+        printBalances(sender, receiver);
+
+        System.out.println("\nTransfer:");
+        System.out.println("ID: " + transfer.getId());
+        System.out.println("Request ID: " + transfer.getRequestId());
+        System.out.println("Status: " + transfer.getStatus());
+
+    }
+
+    private static void printBalances(Wallet sender, Wallet receiver) {
+        System.out.println(
+                "Sender balance: " + sender.getBalance().getAmount()
+        );
+
+        System.out.println(
+                "Receiver balance: " + receiver.getBalance().getAmount()
+        );
     }
 }
